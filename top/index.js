adc978e33c62ac3d684eb9d13691b466ab5904ae
Nikko <xokkinx@gmail.com>
2022-09-11 16:42:35 -0700

    update

---
+++ top/index.js
@@ -0,0 +1,10 @@
+
+// function to convert first letter of a string to uppercase
+
+function lastLetter(str) {
+const last = str.charAt(str.length - 1);
+console.log(last);
+}
+
+lastLetter('abcde');
+
